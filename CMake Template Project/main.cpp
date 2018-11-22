6415a9b9cbf5fe578b71e03671a2142340b83b79
Wouter Kockelkoren <404675@student.saxion.nl>
2018-11-22 15:40:30 +0100

    Start off the map

--- CMake Template Project/main.cpp
+++ CMake Template Project/main.cpp
@@ -1,8 +1,38 @@
 #include <iostream>
+#include <vector>
+
+class Vehicle {
+
+};
+
+class PointOfInterest {
+
+};
+
+class Map {
+	private:
+		std::vector<Vehicle> vehicleList;
+		std::vector<PointOfInterest> poiList;
+
+	public:
+		const int width;
+		const int height;
+	
+	Map::Map(int width, int height) : width(width), height(height) {
+		width;
+		height;
+	}
+};
+
+
 
 int main()
 {
 	printf("Hello World!\n");
 	printf("\n");
+
+	Map factory = Map(10, 10);
+	std::cout << factory.height;
+
 	system("pause");
 }
