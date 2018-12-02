cf2e709f2f3c8abbe4d348fe97f6e6253ee4abaa
Jacco <jaccovanekris@me.com>
2018-12-02 19:56:08 +0100

    Copy the SDL2.dll files to the build directories and run a SDL2 chessboard example

--- CMakeTemplateProject/main.cpp
+++ CMakeTemplateProject/main.cpp
@@ -6,6 +6,72 @@
 #include "SDL.h"
 #undef main
 
+SDL_Window *window;
+SDL_Renderer *renderer;
+SDL_Surface *surface;
+int done;
+
+void DrawChessBoard(SDL_Renderer * renderer)
+{
+	int row = 0, column = 0, x = 0;
+	SDL_Rect rect, darea;
+
+	/* Get the Size of drawing surface */
+	SDL_RenderGetViewport(renderer, &darea);
+
+	for (; row < 8; row++)
+	{
+		column = row % 2;
+		x = column;
+		for (; column < 4 + (row % 2); column++)
+		{
+			SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0xFF);
+
+			rect.w = darea.w / 8;
+			rect.h = darea.h / 8;
+			rect.x = x * rect.w;
+			rect.y = row * rect.h;
+			x = x + 2;
+			SDL_RenderFillRect(renderer, &rect);
+		}
+	}
+}
+
+void loop()
+{
+	SDL_Event e;
+	while (SDL_PollEvent(&e)) {
+
+		/* Re-create when window has been resized */
+		if ((e.type == SDL_WINDOWEVENT) && (e.window.event == SDL_WINDOWEVENT_SIZE_CHANGED)) {
+
+			SDL_DestroyRenderer(renderer);
+
+			surface = SDL_GetWindowSurface(window);
+			renderer = SDL_CreateSoftwareRenderer(surface);
+			/* Clear the rendering surface with the specified color */
+			SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+			SDL_RenderClear(renderer);
+		}
+
+		if (e.type == SDL_QUIT) {
+			done = 1;
+			return;
+		}
+
+		if ((e.type == SDL_KEYDOWN) && (e.key.keysym.sym == SDLK_ESCAPE)) {
+			done = 1;
+			return;
+		}
+	}
+
+	DrawChessBoard(renderer);
+
+	/* Got everything on rendering surface,
+	now Update the drawing image on window screen */
+	SDL_UpdateWindowSurface(window);
+}
+
 struct Boundary {
 	int xUpper = 0;
 	int xLower = 0;
@@ -17,27 +83,6 @@ std::vector<Coordinate> generateListOfPaths(Map *map, Position startPosition, Po
 std::vector<Coordinate> GeneratePath(std::vector<Coordinate> &pathList, Position &start);
 
 int main() {
-
-	SDL_Init(SDL_INIT_VIDEO);
-
-	SDL_Window *window = SDL_CreateWindow(
-		"SDL2Test",
-		SDL_WINDOWPOS_UNDEFINED,
-		SDL_WINDOWPOS_UNDEFINED,
-		640,
-		480,
-		0
-	);
-
-	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
-	SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
-	SDL_RenderClear(renderer);
-	SDL_RenderPresent(renderer);
-
-	SDL_Delay(3000);
-
-	SDL_DestroyWindow(window);
-	SDL_Quit();
 
 	Map factory(10, 10);
 	
@@ -101,6 +146,47 @@ int main() {
 	factory.printMap(vehicles,generatedPath);
 	//factory.printMap();
 	system("pause");
+
+	if (SDL_Init(SDL_INIT_VIDEO) != 0)
+	{
+		SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "SDL_Init fail : %s\n", SDL_GetError());
+		return 1;
+	}
+
+	window = SDL_CreateWindow(
+		"Path Planner",
+		SDL_WINDOWPOS_UNDEFINED,
+		SDL_WINDOWPOS_UNDEFINED,
+		640,
+		480,
+		0
+	);
+	if (!window)
+	{
+		SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Window creation fail : %s\n", SDL_GetError());
+		return 1;
+	}
+
+	surface = SDL_GetWindowSurface(window);
+	renderer = SDL_CreateSoftwareRenderer(surface);
+	if (!renderer)
+	{
+		SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Render creation for surface fail : %s\n", SDL_GetError());
+		return 1;
+	}
+
+	/* Clear the rendering surface with the specified color */
+	SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+	SDL_RenderClear(renderer);
+
+	/* Draw the Image on rendering surface */
+	done = 0;
+	while (!done) {
+		loop();
+	}
+
+	SDL_DestroyWindow(window);
+	SDL_Quit();
 
 	return 0;
 }
