d1610ba1d607a76e469f9d06a1e9e06e3de33bbe
Jacco <jaccovanekris@me.com>
2018-12-06 22:03:23 +0100

    Attempt to solve nanogui build problem

--- CMakeTemplateProject/build_Windows.bat
+++ CMakeTemplateProject/build_Windows.bat
@@ -1,6 +1,20 @@
 cd ext
-git clone --recurse-submodules https://github.com/wjakob/nanogui
+git init
+git submodule add https://github.com/wjakob/nanogui.git nanogui
+git submodule update --init --recursive
+cd nanogui
+
+mkdir build_x86
+cd build_x86
+cmake ..\ -G "Visual Studio 15 2017"
+cmake --build . --target nanogui --config Release
 cd ..
+mkdir build_x64
+cd build_x64
+cmake ..\ -G "Visual Studio 15 2017 Win64"
+cmake --build . --target nanogui --config Release
+cd %~dp0
+
 mkdir build_x86
 cd build_x86
 mkdir Debug
@@ -18,5 +32,10 @@ copy "ext\SDL2-2.0.9\lib\x64\SDL2.dll" "build_x64\Debug\SDL2.dll"
 copy "ext\SDL2-2.0.9\lib\x64\SDL2.dll" "build_x64\Release\SDL2.dll"
 copy "ext\SDL2-2.0.9\lib\x86\SDL2.dll" "build_x86\Debug\SDL2.dll"
 copy "ext\SDL2-2.0.9\lib\x86\SDL2.dll" "build_x86\Release\SDL2.dll"
+
+copy "ext\nanogui\build_x64\Release\nanogui.dll" "build_x64\Debug\nanogui.dll"
+copy "ext\nanogui\build_x64\Release\nanogui.dll" "build_x64\Release\nanogui.dll"
+copy "ext\nanogui\build_x86\Release\nanogui.dll" "build_x86\Debug\nanogui.dll"
+copy "ext\nanogui\build_x86\Release\nanogui.dll" "build_x86\Release\nanogui.dll"
 
 pause
--- CMakeTemplateProject/main.cpp
+++ CMakeTemplateProject/main.cpp
@@ -6,10 +6,36 @@
 #include "Vehicle.h"
 #include "Window.h"
 #include "SDL.h"
-#include <nanogui/nanogui.h>
 #include "time.h"
 
+#include <nanogui/screen.h>
+#include <nanogui/window.h>
+#include <nanogui/layout.h>
+#include <nanogui/button.h>
+
 int main(int argc, char*argv[]){
+
+	nanogui::init();
+
+	nanogui::Screen screen{{600, 420}, "Screen"};
+	nanogui::Window window{&screen, "Window"};
+    window.setPosition({15, 15});
+    window.setLayout(new nanogui::GroupLayout());
+	nanogui::Button *b =new nanogui::Button(&window, "My Button");
+    b->setCallback([] { std::cout << "Button pressed!" << std::endl; });
+
+    screen.performLayout();
+
+    screen.drawAll();
+    screen.setVisible(true);
+
+    nanogui::mainloop();
+
+    nanogui::shutdown();
+    exit(EXIT_SUCCESS);
+
+
+
 	// Timing stuff
 		clock_t this_time = clock();
 		clock_t last_time = this_time;
@@ -48,12 +74,12 @@ int main(int argc, char*argv[]){
 	//path finding algorithm (sample algorithm)
 	std::vector<Coordinate> generatedPath = pathManager.createPath(startPosition, dropOff, factory);
 
-	SDL_Window* window;
+	SDL_Window* mapWindow;
 	SDL_Surface* surface;
 	SDL_Renderer* renderer;
 
 	// Create a window, surface and renderer. Continue when no errors occured, otherwise stop the program
-	if (!loadWindow(&window, 640, 480, &surface, &renderer))
+	if (!loadWindow(&mapWindow, 640, 480, &surface, &renderer))
 		return 0;
 
 	/* Draw the Image on rendering surface */
@@ -67,7 +93,7 @@ int main(int argc, char*argv[]){
 
 				SDL_DestroyRenderer(renderer);
 
-				surface = SDL_GetWindowSurface(window);
+				surface = SDL_GetWindowSurface(mapWindow);
 				renderer = SDL_CreateSoftwareRenderer(surface);
 				/* Clear the rendering surface with the specified color */
 				SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
@@ -102,10 +128,10 @@ int main(int argc, char*argv[]){
 
 		/* Got everything on rendering surface,
 		now Update the drawing image on window screen */
-		SDL_UpdateWindowSurface(window);
+		SDL_UpdateWindowSurface(mapWindow);
 	}
 
-	SDL_DestroyWindow(window);
+	SDL_DestroyWindow(mapWindow);
 	SDL_Quit();
 
 	return 0;
