273851d9ac4d6edaa19b5614b569b50c3a31a945
Jacco <jaccovanekris@me.com>
2019-01-15 00:54:49 +0100

    Cleaned files and added some comments

--- CMakeTemplateProject/include/Window.cpp
+++ CMakeTemplateProject/include/Window.cpp
@@ -1,12 +1,15 @@
 #include "Window.h"
 
 bool loadWindow(SDL_Window** window, size_t width, size_t height, SDL_Surface** surface, SDL_Renderer** renderer) {
+	
+	// Initialize SDL2
 	if (SDL_Init(SDL_INIT_VIDEO) != 0)
 	{
 		SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "SDL_Init fail : %s\n", SDL_GetError());
 		return false;
 	}
 
+	// Create a SDL window
 	*window = SDL_CreateWindow(
 		"Path Planner",
 		50,
@@ -15,30 +18,33 @@ bool loadWindow(SDL_Window** window, size_t width, size_t height, SDL_Surface** 
 		(int) height,
 		0
 	);
-	if (!*window)
+	if (!*window) /* Check whether creating SDL window was successful */
 	{
 		SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Window creation fail : %s\n", SDL_GetError());
 		return false;
 	}
 
+	// Create a SDL surface and renderer
 	*surface = SDL_GetWindowSurface(*window);
 	*renderer = SDL_CreateSoftwareRenderer(*surface);
-	if (!renderer)
+	if (!renderer) /* Check whether creating the render was successful */
 	{
 		SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Render creation for surface fail : %s\n", SDL_GetError());
 		return false;
 	}
 
-	/* Clear the rendering surface with the specified color */
+	// Clear the rendering surface with the specified color
 	SDL_SetRenderDrawColor(*renderer, 0xFF, 0xFF, 0xFF, 0xFF);
 	SDL_RenderClear(*renderer);
 	return true;
 }
 
 void updateScreen(SDL_Renderer *renderer, SDL_Window *mapWindow, Map &map, std::vector<Vehicle> vehicles) {
+	
+	// Update the map
 	try {
 		map.printMap(renderer, vehicles);
-	}
+	} /* Catch possible exceptions */
 	catch (std::exception const& e) {
 		std::cout << e.what();
 	}
